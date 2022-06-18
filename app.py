ca4f73aae2d6d8730476467d082abd4ef56f5c09
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2022-06-18 14:08:02 +0530

    add logging and exception part successfully

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+from logging import exception
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    logging.info("Testing the logging module")
+    logging.info("App is started")
+    try:
+        raise Exception('Demo exception is added')
+    except Exception as e:
+        housing_exception = HousingException(e, sys)
+        logging.info(housing_exception.error_message)
     return '<h1>Hello Flask</h1>'
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ REQUIREMENT_FILE_NAME = "requirements.txt"
 
 def get_requirements_list()->List[str]:
     '''
-    Description : 
+    Description : This function is going to return list of requirement 
+    mention in requirements.txt file.
 
-    return 
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file.
     '''
     with open(REQUIREMENT_FILE_NAME) as REQUIREMENT_FILE:
         return REQUIREMENT_FILE.readlines().remove('-e .')
